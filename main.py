49cc2b5d56734245a18ccd329ab2b919a57692e7
Lars <lars.eriksson.mail@gmail.com>
2022-02-25 20:42:23 +0100

    Added a loop I forgot. derp.

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
 user_input = input("What do you want natoized?:").upper()
+def generate_phon():
+    user_input = input("What do you want natoized?:").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Only letters in the alphabet is allowed")
+        generate_phon()
+    else:
+        print(output_list)
 
-try:
-    output_list = [phonetic_dict[letter] for letter in user_input]
-except KeyError:
-    print("Only letters in the alphabet is allowed")
-else:
-    print(output_list)
+
+
+generate_phon()
